5d3a8bd8d39075f51bcb05456c253fc69c700eac
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-09-05 16:36:44 +0545

    new

--- examples/examples-browser/server.js
+++ examples/examples-browser/server.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const viewsDir = path.join(__dirname, "views");
 app.use(express.static(viewsDir));
+app.use(express.static(path.join(__dirname, "views")));
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(express.static(path.join(__dirname, "../images")));
 app.use(express.static(path.join(__dirname, "../media")));
@@ -30,7 +31,7 @@ app.use(express.static(path.join(__dirname, "../../dist")));
 app.get("/", (req, res) => res.sendFile(path.join(viewsDir, "login.html")));
 
 app.post("/login", (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   const { username, password } = req.body;
 
   const validUsername = "admin";
@@ -44,68 +45,85 @@ app.post("/login", (req, res) => {
   }
 });
 
-function isAuthenticated(req, res, next) {
+// Log Out
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error during logout:", err);
+      return res.status(500).send("Error Logging Out");
+    }
+    console.log("logout")
+    res.redirect("/");
+  });
+});
+
+
+
+// Protect the routes
+function checkAuth(req, res, next) {
   if (req.session.isLoggedIn) {
     return next();
+  } else {
+    res.redirect("/");
   }
-  res.redirect("/");
 }
-
 
 // Only allow access to these routes if logged in
 
-app.get("/", (req, res) => res.redirect("/face_detection"));
-app.get("/face_detection", (req, res) =>
+// app.get("/", (req, res) => res.redirect("/face_detection"));
+
+app.get("/face_detection", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "faceDetection.html"))
 );
-app.get("/face_landmark_detection", (req, res) =>
+app.get("/face_landmark_detection", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "faceLandmarkDetection.html"))
 );
-app.get("/face_expression_recognition", (req, res) =>
+app.get("/face_expression_recognition", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "faceExpressionRecognition.html"))
 );
-app.get("/age_and_gender_recognition", (req, res) =>
+app.get("/age_and_gender_recognition", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "ageAndGenderRecognition.html"))
 );
-app.get("/face_extraction", (req, res) =>
+app.get("/face_extraction", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "faceExtraction.html"))
 );
-app.get("/face_recognition", (req, res) =>
+app.get("/face_recognition", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "faceRecognition.html"))
 );
-app.get("/video_face_tracking", (req, res) =>
+app.get("/video_face_tracking", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "videoFaceTracking.html"))
 );
-app.get("/webcam_face_detection", (req, res) =>
+app.get("/webcam_face_detection", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "webcamFaceDetection.html"))
 );
-app.get("/webcam_face_landmark_detection", (req, res) =>
+app.get("/webcam_face_landmark_detection", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "webcamFaceLandmarkDetection.html"))
 );
-app.get("/webcam_face_expression_recognition", (req, res) =>
+app.get("/webcam_face_expression_recognition", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "webcamFaceExpressionRecognition.html"))
 );
-app.get("/webcam_age_and_gender_recognition", (req, res) =>
+app.get("/webcam_age_and_gender_recognition", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "webcamAgeAndGenderRecognition.html"))
 );
-app.get("/bbt_face_landmark_detection", (req, res) =>
+app.get("/bbt_face_landmark_detection", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "bbtFaceLandmarkDetection.html"))
 );
-app.get("/bbt_face_similarity", (req, res) =>
+app.get("/bbt_face_similarity", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "bbtFaceSimilarity.html"))
 );
-app.get("/bbt_face_matching", (req, res) =>
+app.get("/bbt_face_matching", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "bbtFaceMatching.html"))
 );
-app.get("/bbt_face_recognition", (req, res) =>
+app.get("/bbt_face_recognition", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "bbtFaceRecognition.html"))
 );
-app.get("/batch_face_landmarks", (req, res) =>
+app.get("/batch_face_landmarks", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "batchFaceLandmarks.html"))
 );
-app.get("/batch_face_recognition", (req, res) =>
+app.get("/batch_face_recognition", checkAuth, (req, res) =>
   res.sendFile(path.join(viewsDir, "batchFaceRecognition.html"))
 );
+
 
 app.post("/fetch_external_image", async (req, res) => {
   const { imageUrl } = req.body;
